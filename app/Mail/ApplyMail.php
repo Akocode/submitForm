43d5d70af4f82b8e9548fadb55558addd0c519a5
emizy <adenitiree@gmail.com>
2020-08-12 09:11:49 +0100

    form submit

--- app/Mail/ApplyMail.php
+++ app/Mail/ApplyMail.php
@@ -10,7 +10,7 @@ use Illuminate\Queue\SerializesModels;
 class ApplyMail extends Mailable
 {
     use Queueable, SerializesModels;
-    protected $data;
+    public $data;
     /**
      * Create a new message instance.
      *
@@ -30,11 +30,11 @@ class ApplyMail extends Mailable
     public function build()
     {
         return $this->view('email/email')
-            ->with(['firstname'=>$this->data->firstname,
-                    'lastname'=>$this->data->lastname,
-                    'email'=>$this->data->email,
-                    'number'=>$this->data->number,
-                    'addition'=>$this->data->addition])
+            ->with(['firstname'=>$this->data['firstname'],
+                    'lastname'=>$this->data['lastname'],
+                    'email'=>$this->data['email'],
+                    'number'=>$this->data['number'],
+                    'addition'=>$this->data['addition']])
             ->from('site1350@gmail.com')
             ->subject('New Application')
             ->attach($this->data['file']->getRealPath(),
